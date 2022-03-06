74424597c0b8694171740457a32dd158876b92d2
Victor Hugo Flores Vargas <70537860+Hugo-fv@users.noreply.github.com>
2022-03-06 15:57:51 -0600

    Funcion EDITAR añadida
    La función Editar del CRUD ya se puede utilizar sin problemas

--- Web-Server/controllers/crud.js
+++ Web-Server/controllers/crud.js
@@ -1,3 +1,4 @@
+const res = require('express/lib/response');
 const conexion = require('../database/db');
 const mysql = require('../database/db');
 
@@ -6,13 +7,29 @@ exports.save = (req,res) =>{
     const nombre = req.body.nombre;
     const unidad = req.body.unidad;
 
-    conexion.query('INSERT INTO DatosGenerales SET ?', {uid:user, nombre:nombre, n_camion:unidad}, (err, results)=>{
+    conexion.query('INSERT INTO users SET ?', {uid:user, nombre:nombre, n_camion:unidad}, (err, results)=>{
         if (err){
             console.log(err);
         }else{
             res.redirect('/crud');
         }
     })
-
 }
+
+exports.update = (req,res)=>{
+    const id = req.body.id;
+    const uid = req.body.user;
+    const nombre = req.body.nombre;
+    const unidad = req.body.unidad;
+
+    conexion.query('UPDATE users SET ? WHERE id = ?',[{uid:uid, nombre:nombre, n_camion:unidad}, id], (error, results)=>{
+        if (error){
+            throw error;
+        }else{
+            res.redirect('/crud');
+        }
+    })
+}
+
+
 
