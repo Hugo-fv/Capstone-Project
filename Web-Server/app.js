50ec8dcc9b6456473aaba97be2375a71303e13b5
Victor Hugo Flores Vargas <70537860+Hugo-fv@users.noreply.github.com>
2022-03-08 20:22:27 -0600

    Uso de Datatables
    Uso de Datatables para el registro

--- Web-Server/app.js
+++ Web-Server/app.js
@@ -1,3 +1,32 @@
+/*
+Servidor Web para el sistema de autobuses.
+Por: Victor Hugo Flores Vargas
+Fecha de creacion: 25 de febrero.
+
+El proyecto esta organizado de la siguiente forma.
+
+1. app.js: Contiene los metodos necesarios para iniciar un servidor con Express
+    y EJS (motor de plantillas de Javascript)
+2. router.js: Maneja todas las peticiones GET para el correcto funcionamiento de la
+    pagina web.
+3. Carpeta 'views': contiene las plantillas EJS que permiten visualizar cada pagina
+    que conforma la pagina web.
+4. Carpeta 'controllers': contiene el archivo 'crud.js', el cual maneja la creacion y 
+    edición del CRUD
+5. Carpeta 'database': contiene la conexión con la base de datos 'RegistroAutobuses'
+
+El proyecto utiliza las siguientes herramientas
+
+1. Framework: Bootstrap 5
+2. Uso de iconos: BoxIcons
+3. Manejo de la tabla de registro de tiempos: Datatables
+4. Manejo de graficos: ChartJS
+5. Extras: Jquery y Poppet
+6. Servidor: NodeJS con Express
+
+*/
+
+
 const express = require('express');
 const app = express();
 
--- Web-Server/router.js
+++ Web-Server/router.js
@@ -32,9 +32,32 @@ router.get('/edit/:id', (req,res)=>{
     })
 })
 
+router.get('/delete/:id', (req, res)=>{
+    const id = req.params.id;
+    conexion.query('DELETE FROM users WHERE id= ? ', [id], (error, results)=>{
+        if (error){
+            throw error;
+        }else{
+            res.redirect('/crud')
+        }
+    });
+})
+
+
+
+router.get('/prueba', (req,res)=>{
+    conexion.query('SELECT DG.uid, nombre, n_camion, id_estacion, DATE_FORMAT(FROM_UNIXTIME(h_entrada), "%H:%i") AS Entrada, DATE_FORMAT(FROM_UNIXTIME(h_salida), "%H:%i") AS Salida, DATE_FORMAT(FROM_UNIXTIME(h_entrada), "%d-%m-%Y") AS Fecha FROM users DG JOIN estacion HR ON DG.uid = HR.uid', (error, results)=>{
+        if (error){
+            throw error;
+        }else{
+            res.send(results);
+        }
+    });
+});
+
 router.get('/registro', (req,res)=>{
     res.render('registro');
-});
+})
 
 
 
